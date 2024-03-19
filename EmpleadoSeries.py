823e7518248fc511e25274240473b62638d469f9
cmendoza-dev <carlos.mendoza.s@edu.pe>
2024-03-19 08:31:31 -0500

    Carga de archivos

---
+++ EmpleadoSeries.py
@@ -0,0 +1,4 @@
+#Genere un arreglo de 6 elementos de texto renombrando sus índices e imprima los datos
+
+
+#Genere un arreglo de 3 elementos de texto con sus 3 valores numéricos e imprima los datos
